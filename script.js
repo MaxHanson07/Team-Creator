e6c005b48b6133c60eea8fbc4d7899662f08ef45
Max Hanson <mthanson2@gmail.com>
2020-12-16 11:05:27 -0800

    psuedocoded the javascript

---
+++ script.js
@@ -0,0 +1,40 @@
+// Add players into different tiers
+
+    // Store tiers in individual arrays
+
+    // Display names in correct div
+
+    // Delete button
+
+    // Input to add a player to tier
+
+    // Add a save button to save the inputted players to local storage
+
+// Randomly generate 10-man teams
+
+    // Get user input of number of teams
+
+    // Get user input of number of players from each tier
+    
+    // Create new array for each team to store that team's players in using for loop
+
+    // Iterate over each tier array to get desired amount of players from each tier
+
+        // The amount of iterations is the amount of players specified to be selected from each tier times the number of teams
+
+        // Use Math.random to choose a random player from that tier
+
+        // Assign 1st selected player to team 1, 2nd to team two, etc and repeat until all teams have correct number of players from that tier
+
+        // Repeat the process for all tiers
+
+    // Display each team in the proper div
+
+    // Add validation
+
+        // No errors
+
+        // Handle position shortages
+
+    // Bonus: Allow user to manually customize teams
+
